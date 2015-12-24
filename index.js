8879e251e602b604ae83729a807149f2f9778feb
sorokinvladimir <fafafa@e1.ru>
2015-12-24 08:27:52 +0500

    1

---
+++ index.js
@@ -0,0 +1,18 @@
+function imgLoaded(img){
+    var $img = $(img);
+ 
+    $img.parent().addClass('loaded');
+};
+
+
+  var form = document.getElementsByClassName('b-form')[0];
+
+    var name = document.querySelector('.b-form__name');
+    name.getAttribute('placeholder');
+    name.removeAttribute('placeholder');
+
+        <div class='page' data-server-time='01/09/2007'>
+    </div>
+
+    var page = document.querySearch('page');
+    page.getAttribute('data-search-time');
